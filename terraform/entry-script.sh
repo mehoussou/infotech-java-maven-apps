c75e23e5e6eee916cbd4cf59090bc5734664d44c
DevEngineer <devopgit@gmail.com>
2023-01-19 23:07:43 -0700

    adding terraform code in jenkins pipeline

---
+++ terraform/entry-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+
+# install docker-compose into new instance
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#docker command to run nginx
+#docker run -d -p 8080:80 nginx
---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "vpc_cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "subnet_cidr_block" {
+     default = "10.0.10.0/24"
+}
+variable "avail_zone" {
+     default = "us-east-2"
+}
+variable "env_prefix" {
+     default = "infotech"
+}
+variable "my_ip" {
+     default = "75.71.114.225"
+}
+variable "instance_type" {
+     default = "t2.micro"
+}
